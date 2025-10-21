3486a18a4ae69b8bbf2b6e479d4885fe5f559b43
LorraineHLi <lihanwen1030@outlook.com>
2025-10-21 14:50:30 +0100

    part1 question1 done

--- inverse_geometry.py
+++ inverse_geometry.py
@@ -15,12 +15,63 @@ from config import CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET
 
 from tools import setcubeplacement
 
+import time
+from scipy.optimize import fmin_bfgs
+from setup_meshcat import updatevisuals
+
 def computeqgrasppose(robot, qcurrent, cube, cubetarget, viz=None):
     '''Return a collision free configuration grasping a cube at a specific location and a success flag'''
     setcubeplacement(robot, cube, cubetarget)
     #TODO implement
-    print ("TODO: implement me")
-    return robot.q0, False
+    
+    #Get frame IDs for robot hands
+    handidL = robot.model.getFrameId(LEFT_HAND)
+    handidR = robot.model.getFrameId(RIGHT_HAND)
+    
+    #Get target hook poses from cube
+    oMhookL = getcubeplacement(cube, LEFT_HOOK)
+    oMhookR = getcubeplacement(cube, RIGHT_HOOK)
+    
+    qBias = robot.q0.copy() 
+    COLLISION_WEIGHT = 1e5
+    POSTURAL_BIAS = 1e-2
+    
+    def cost(q):
+        #project q to joint limits
+        qProj = projecttojointlimits(robot, q)
+        pin.framesForwardKinematics(robot.model, robot.data, qProj)
+        oMhandL = robot.data.oMf[handidL]
+        oMhandR = robot.data.oMf[handidR]
+        errL = norm(pin.log(oMhandL.inverse() * oMhookL).vector) ** 2
+        errR = norm(pin.log(oMhandR.inverse() * oMhookR).vector) ** 2
+        
+        collisionCost = COLLISION_WEIGHT * collision(robot, qProj)
+        posturalBias = POSTURAL_BIAS * norm(qProj - qBias) ** 2
+        return errL + errR + collisionCost + posturalBias
+    
+    def callback(q):
+        if viz:
+            qProj = projecttojointlimits(robot, q)
+            updatevisuals(viz, robot, cube, qProj)
+            time.sleep(1e-2)
+    
+    #optimisation
+    q0 = qcurrent.copy()
+    qOpt = fmin_bfgs(cost, q0, callback=callback)
+    qFinal = projecttojointlimits(robot, qOpt)
+    
+    #recalculate cost
+    pin.framesForwardKinematics(robot.model, robot.data, qFinal)
+    oMhandL = robot.data.oMf[handidL]
+    oMhandR = robot.data.oMf[handidR]
+    grasp_error = norm(pin.log(oMhandL.inverse() * oMhookL).vector) ** 2 + \
+                  norm(pin.log(oMhandR.inverse() * oMhookR).vector) ** 2
+    
+    GRASP_TOLERANCE = 1e-4
+    success = (grasp_error < GRASP_TOLERANCE) and (not collision(robot, qFinal))
+    
+    #print ("TODO: implement me")
+    return qFinal, success
             
 if __name__ == "__main__":
     from tools import setupwithmeshcat
